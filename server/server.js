472a5d8f622501c9c6de65b8c304c2b12354227a
Nadia <nadia@fake.com>
2023-10-03 12:01:43 -0400

    /api/characters endpoint pulls from local mongodb.

--- server/server.js
+++ server/server.js
@@ -4,11 +4,68 @@ const bodyParser = require('body-parser');
 
 var url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
+const dbName = "swapi";
 
 var app = express();
 app.use(bodyParser.json());
 
+app.get("/api/characters", async (req, res) => {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection('characters');
+
+    const characters = await collection.find({}).toArray();
+
+    console.log("Sending characters: ", characters);
+    res.send({"characters": characters});
+});
+
+app.get("/api/films", (req, res) => {
+    console.log("Sending fake test object");
+    res.send({fakeKey: 'FakeData'});
+});
+
 app.get("/api/planets", (req, res) => {
+    console.log("Sending fake test object");
+    res.send({fakeKey: 'FakeData'});
+});
+
+app.get("/api/characters/:id", (req, res) => {
+    console.log("Sending fake test object");
+    res.send({fakeKey: 'FakeData'});
+});
+
+app.get("/api/films/:id", (req, res) => {
+    console.log("Sending fake test object");
+    res.send({fakeKey: 'FakeData'});
+});
+
+app.get("/api/planets/:id", (req, res) => {
+    console.log("Sending fake test object");
+    res.send({fakeKey: 'FakeData'});
+});
+
+app.get("/api/films/:id/characters", (req, res) => {
+    console.log("Sending fake test object");
+    res.send({fakeKey: 'FakeData'});
+});
+
+app.get("/api/films/:id/planets", (req, res) => {
+    console.log("Sending fake test object");
+    res.send({fakeKey: 'FakeData'});
+});
+
+app.get("/api/characters/:id/films", (req, res) => {
+    console.log("Sending fake test object");
+    res.send({fakeKey: 'FakeData'});
+});
+
+app.get("/api/planets/:id/films", (req, res) => {
+    console.log("Sending fake test object");
+    res.send({fakeKey: 'FakeData'});
+});
+
+app.get("/api/planets/:id/characters", (req, res) => {
     console.log("Sending fake test object");
     res.send({fakeKey: 'FakeData'});
 });
