93badd3d06642092ef7809cdfa34b4fe28378764
Nadia <nadia@fake.com>
2023-10-03 21:10:58 -0400

    get list of characters in a film

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.get("/api/characters", async (req, res) => {
     const characters = await collection.find({}).toArray();
 
     console.log("Sending characters: ", characters);
-    res.send({"characters": characters});
+    res.send({ "characters": characters });
     client.close();
 });
 
@@ -31,7 +31,7 @@ app.get("/api/films", async (req, res) => {
     const films = await collection.find({}).toArray();
 
     console.log("Sending films: ", films);
-    res.send({"films": films});
+    res.send({ "films": films });
     client.close();
 });
 
@@ -40,7 +40,7 @@ app.get("/api/planets", async (req, res) => {
     const planets = await collection.find({}).toArray();
 
     console.log("Sending planets: ", planets);
-    res.send({"planets": planets});
+    res.send({ "planets": planets });
     client.close();
 });
 
@@ -72,28 +72,60 @@ app.get("/api/planets/:id", async (req, res) => {
 });
 
 app.get("/api/films/:id/characters", async (req, res) => {
-    console.log("Sending fake test object");
-    res.send({fakeKey: 'FakeData'});
+    const collection = await mongoConnect('films_characters');
+    const charactersInFilm = await collection.aggregate([
+        {
+            $match: {
+                $and: [{ film_id: +req.params.id }]
+            }
+        }, {
+            $lookup: {
+                from: "characters",
+                localField: "character_id",
+                foreignField: "id",
+                as: "character"
+            }
+        }, {
+            $unwind: "$character"
+        }, {
+            $project: {
+                id: "$character.id",
+                name: "$character.name",
+                gender: "$character.gender",
+                skin_color: "$character.skin_color",
+                hair_color: "$character.hair_color",
+                height: "$character.height",
+                eye_color: "$character.eye_color",
+                mass: "$character.mass",
+                homeworld: "$character.homeworld",
+                birth_year: "$character.birth_year",
+            }
+        }
+    ]).toArray();
+
+    console.log("Sending charactersInFilm: ", charactersInFilm);
+    res.send({ "charactersInFilm": charactersInFilm });
+    client.close();
 });
 
 app.get("/api/films/:id/planets", async (req, res) => {
     console.log("Sending fake test object");
-    res.send({fakeKey: 'FakeData'});
+    res.send({ fakeKey: 'FakeData' });
 });
 
 app.get("/api/characters/:id/films", async (req, res) => {
     console.log("Sending fake test object");
-    res.send({fakeKey: 'FakeData'});
+    res.send({ fakeKey: 'FakeData' });
 });
 
 app.get("/api/planets/:id/films", async (req, res) => {
     console.log("Sending fake test object");
-    res.send({fakeKey: 'FakeData'});
+    res.send({ fakeKey: 'FakeData' });
 });
 
 app.get("/api/planets/:id/characters", async (req, res) => {
     console.log("Sending fake test object");
-    res.send({fakeKey: 'FakeData'});
+    res.send({ fakeKey: 'FakeData' });
 });
 
 // start the rest service
